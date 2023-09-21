107b47a6f7f127f7a7d045b28bf2ccd949ae9eb7
Jonathan <joathan22222001428337@gmail.com>
2023-09-21 17:29:51 +0800

    first commit

---
+++ Typescripttest/test.ts
@@ -0,0 +1,13 @@
+/**create web server */
+
+import * as http from "http";
+import * as url from "url";
+import * as fs from "fs";
+import * as path from "path";
+import * as OS from "os";
+
+import express from "express";
+
+const app = express();
+
+/**write code to  */
---
+++ pythontest.py
@@ -0,0 +1,9 @@
+def sum(a,b):
+    a=1
+    b=2
+    print(a+b)
+
+c=sum(10,11)
+print(c)
+
+£merge two column in pandas
